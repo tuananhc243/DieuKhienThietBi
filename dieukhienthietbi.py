215cc3664d9349aaa53085dc521249c619cc073a
tuananhc243 <chutuananh2403@gmail.com>
2025-03-27 16:53:08 +0700

    Add files via upload

---
+++ dieukhienthietbi.py
@@ -0,0 +1,121 @@
+from tkinter  import *
+import paho.mqtt.client as mqtt
+
+# Khai báo thông tin broker và topic
+pub = "S" #chủ đề gửi thông tin đến esp
+sub = "S_tt"# chủ đề nhận thông tin từ esp
+pub1 = "M"# chủ đề quạt
+sub1 = "M_tt"#
+
+broker_address = "9ba9df8b51f04323ae32907b8d102e89.s1.eu.hivemq.cloud"
+port = 8883 
+username = "tuananhc"
+password = "Abc123456789"
+
+# Kết nối đến HiveMQ broker
+client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)  # Sử dụng API mới
+client.username_pw_set(username, password)#kết nối tk mk trước rồi mới đến địa chỉ và cổng
+client.tls_set()  # Kết nối bảo mật SSL/TLS
+client.connect(broker_address, port)
+client.subscribe(sub)
+client.subscribe(pub)
+client.subscribe(sub1)
+client.subscribe(pub1)
+
+# Biến trạng thái
+is_on = False  # Trạng thái hiện tại của nút nhấn
+
+# Xử lý khi nhận tin nhắn từ MQTT được gủi từ esp
+def on_message(client, userdata, message):
+    global is_on
+    msg = message.payload.decode("utf-8")
+    if msg == "Den Bat":
+        on_button.config(image=on)
+        is_on = True
+    elif msg == "Den Tat" :
+        on_button.config(image=off)
+        is_on = False 
+    window.update()
+    print(f"[Nhận từ MQTT] Tin nhắn: {msg}")  # Debug
+client.message_callback_add(sub, on_message)
+
+def on_message1(client, userdata, message):
+    msg = message.payload.decode("utf-8")
+    if msg == "Muc_0":
+        change_color()
+        button_muc0.config(bg="orange")
+    if msg == "Muc_1":
+        change_color()
+        button_muc1.config(bg="orange") 
+    if msg == "Muc_2":
+        change_color()
+        button_muc2.config(bg="orange")
+    if msg == "Muc_3":
+        change_color()
+        button_muc3.config(bg="orange")    
+    window.update()
+   
+client.message_callback_add(sub1, on_message1)  # Gán hàm xử lý tin nhắn
+
+#khơi tạo GUI(khung giao diện điều khiển)
+window = Tk()
+window.title('giao diện điều khiển')
+window.geometry('500x300+400+300')#khung dài(x) * rộng (y), vị trí x = 400 y=300
+
+# Hàng 1: Tiêu đề chính
+label_title = Label(window, text="Điều khiển thiết bị", fg="Blue", font=("Helvetica", 18, "bold"))
+label_title.pack(pady=10)
+
+# Hàng 2: Tiêu đề điều khiển bóng đèn
+my_label = Label(window, text = "Bật tắt bóng đèn", fg="Green", font= ("Helvetica", 18))
+my_label.pack()
+
+#gắn hình ảnh nút nhấn vào khung và xử lý nút nhấn giao diện điều khiển hàng 3
+on = PhotoImage(file= "images_button/on.png")
+off = PhotoImage(file = "images_button/off.png")
+def switch():
+    global is_on
+    if is_on:
+        on_button.config(image = off)
+        is_on = False
+        client.publish(pub, "0")  # Gửi lệnh "0" đến ESP32 thông qua topic S
+    else:
+        on_button.config(image = on)
+        is_on = True
+        client.publish(pub, "1") # Gửi lệnh "1" đến ESP32 thông qua topic S
+    print(f"[Nút nhấn] Trạng thái: {'on' if is_on else 'off'}")  # Debug
+
+on_button = Button(window, image= off, bd = 0, command= switch)
+on_button.pack(pady=5)
+
+# Hàng 4: Tiêu đề điều khiển quạt
+label_quat = Label(window, text="Điều khiển quạt", fg="Green", font=("Helvetica", 18))
+label_quat.pack(pady=10)
+
+# Hàng 5: 4 nút điều khiển quạt
+def change_color():
+    button_muc0.config(bg="white")
+    button_muc1.config(bg="white")
+    button_muc2.config(bg="white")
+    button_muc3.config(bg="white")
+
+
+def switch1(level, button):
+    change_color()
+    button.config(bg= "orange")
+    client.publish(pub1, str(level))
+    print(f"[MQTT] gửi tín hiệu: Quạt mức {level}") 
+
+button_muc0 = Button(window, text="Mức 1", width=10, height=2, command=lambda: switch1(96, button_muc0))
+button_muc1 = Button(window, text="Mức 2", width=10, height=2, command=lambda: switch1(168, button_muc1))
+button_muc2 = Button(window, text="Mức 3", width=10, height=2, command=lambda: switch1(255, button_muc2))
+button_muc3 = Button(window, text="Tắt Quạt", width=10, height=2, command=lambda: switch1(0, button_muc3))
+
+button_muc0.pack(side=LEFT, padx=5, pady=5) #pad x (padx =5 khoảng cách trái/phải 5 pixel)
+button_muc1.pack(side=LEFT, padx=5, pady=5)#pad y (pady = khoảng cách trên dưới)
+button_muc2.pack(side=LEFT, padx=5, pady=5)
+button_muc3.pack(side=LEFT, padx=5, pady=5)
+
+
+client.loop_start()
+window.mainloop()
---
+++ pwm.ino
@@ -0,0 +1,138 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <WiFiClientSecure.h>
+
+#define sub "S" 
+#define pub "S_tt"
+#define sub1 "M" 
+#define pub1 "M_tt"
+
+#define button_d 3 
+#define button_q 32 
+#define RL 1 
+#define RL1 14 
+
+const char* ssid = "Truong Thi Ha";  
+const char* password = "25101973"; 
+
+const char* mqtt_server = "9ba9df8b51f04323ae32907b8d102e89.s1.eu.hivemq.cloud";
+const int mqtt_port = 8883;
+const char* mqtt_username = "tuananhc"; 
+const char* mqtt_password = "Abc123456789"; 
+
+WiFiClientSecure espClient;
+PubSubClient client(espClient);
+
+int RL_tt = 0; 
+int pwm_muc[] = {0, 64, 168, 255};
+int current_muc = 0;
+bool lastButtonState = HIGH;
+
+void setup_wifi(){
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) { 
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println("WiFi connected!");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    String clientId = "ESP32-" + String(random(1000, 9999)); 
+    if (client.connect(clientId.c_str(), mqtt_username, mqtt_password)) {
+      Serial.println("MQTT connected");
+      client.subscribe(sub);
+      client.subscribe(sub1);
+    } else {
+      delay(5000);
+    }
+  }
+}
+
+void callback(char* topic, byte* payload, unsigned int length) { 
+  if ((char)payload[0] == '1') {
+    client.publish(pub, "Den Bat");
+    digitalWrite(RL, HIGH);
+    RL_tt = 1;
+  } else {
+    client.publish(pub, "Den Tat");
+    digitalWrite(RL, LOW);
+    RL_tt = 0;
+  }
+}
+
+void callback1(char* topic, byte* payload, unsigned int length) { 
+  String message = "";
+  for (int i = 0; i < length; i++) {
+    message += (char)payload[i];
+  }
+  int pwm_value = message.toInt(); 
+  pwm_value = constrain(pwm_value, 0, 255); 
+  int new_muc = 0;
+  for (int i = 0; i < 4; i++) {
+    if (pwm_value == pwm_muc[i]) {
+      new_muc = i;
+      break;
+    }
+  }
+  current_muc = new_muc;
+}
+
+void DK_nutan() {
+  if (digitalRead(button_d) == LOW) {
+    delay(200);
+    RL_tt = !RL_tt;
+    digitalWrite(RL, RL_tt);
+    client.publish(pub, RL_tt ? "Den Bat" : "Den Tat");
+  }
+}
+
+void handleButton() {
+  bool buttonState = digitalRead(button_q);
+  if (buttonState == LOW && lastButtonState == HIGH) {
+    delay(200);
+    current_muc = (current_muc + 1) % 4;
+    String response = "Muc_" + String(current_muc);
+    client.publish(pub1, response.c_str());
+  }
+  lastButtonState = buttonState;
+}
+
+void generatePWM(int pin, int dutyCycle, int frequency) {
+  int period = 1000000 / frequency; //chu kì (micro giay (ms)) của xung pwm 1s = 1.000.000 / tần số
+  int highTime = (period * dutyCycle) / 255; // thời gian bật =  chu kỳ xung cao 
+  int lowTime = period - highTime;
+  digitalWrite(pin, HIGH);
+  delayMicroseconds(highTime);
+  digitalWrite(pin, LOW);
+  delayMicroseconds(lowTime);
+}
+
+void setup() {
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, mqtt_port);
+  espClient.setInsecure();  
+  client.setCallback(callback);
+  client.setCallback(callback1);
+
+  pinMode(button_d, INPUT_PULLUP);
+  pinMode(button_q, INPUT_PULLUP);  
+  pinMode(RL, OUTPUT);
+  pinMode(RL1, OUTPUT);
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  DK_nutan();
+  handleButton();
+  generatePWM(RL1, pwm_muc[current_muc], 5000);
+  client.loop();
+}
